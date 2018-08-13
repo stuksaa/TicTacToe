0d2d81754ea257ce9881a8b39fb6851fce779c88
Békési László <stuksaa@gmail.com>
2018-08-13 23:02:14 +0200

    check game status

--- Game.py
+++ Game.py
@@ -1,6 +1,7 @@
 class TicTacToe:
 
     board_size = 3
+    nof_moves = 0
 
     def __init__(self):
         self.tictactoe_board = [['.' for x in range(self.board_size)] for y in range(self.board_size)]
@@ -14,4 +15,42 @@ class TicTacToe:
 
     def set_move(self, player, r_coord, c_coord):
         self.tictactoe_board[r_coord][c_coord] = player
+        
+        check_game(player, r_coord, c_coord)
 
+        self.nof_moves++
+
+
+    def check_game(self, player, r_coord, c_coord):
+        #check row
+        for i in range(self.board_size):
+            if(self.tictactoe_board[r_coor][i]!=player):
+                break
+            if i == self.board_size-1:
+                #player nyert
+        
+        #check column
+        for i in range(self.board_size):
+            if(self.tictactoe_board[i][c_coord]!=player):
+                break
+            if i == self.board_size-1:
+                #player nyert
+        
+        #check diagonal
+        if r_coord == c_coord:
+            for i in range(self.board_size):
+                if self.tictactoe_board[i][i]!=player:
+                    break
+                if i == self.board_size-1
+                    #player nyert
+
+        #check antidiagonal
+        if r_coord + c_coord == self.board_size-1:
+            for i in rang(self.board_size):
+                if self.tictactoe_board[i][self.board_size-1-i]!=player
+                    break
+                if i == self.board_size-1:
+                    #player nyert
+        
+        if self.nof_moves == self.board_size ** 2:
+            #dontetlen
