1087b234d1a37a1e6e6131299dadd6c93e518774
stuksaa <stuksaa@gmail.com>
2018-08-17 08:21:15 +0200

    multiplayer

--- Game.py
+++ Game.py
@@ -1,6 +1,5 @@
 class TicTacToe:
- 
-    board_size = 1
+    board_size = 3
     nof_moves = 0
 
     def __init__(self):
@@ -13,44 +12,54 @@ class TicTacToe:
                 print(self.tictactoe_board[i][j], end=' ')
             print()
 
-    def set_move(self, player, r_coord, c_coord):
-        self.tictactoe_board[r_coord][c_coord] = player
-        
-        check_game(player, r_coord, c_coord)
+    def check_position(self, r_coord, c_coord):
+        if self.tictactoe_board[r_coord][c_coord] == '.':
+            return True
+        else:
+            return False
 
-        self.nof_moves++
-
+    def increase_moves(self):
+        self.nof_moves = self.nof_moves + 1
 
     def check_game(self, player, r_coord, c_coord):
-        #check row
-        for i in range(self.board_size):
-            if(self.tictactoe_board[r_coor][i]!=player):
+        self.increase_moves()
+        # check row
+        for j in range(self.board_size):
+            if self.tictactoe_board[r_coord][j] != player:
                 break
-            if i == self.board_size-1:
-                #player nyert
-        
-        #check column
+            if j == self.board_size - 1:
+                print(player, ' nyert!')
+                return True
+
+        # check column
         for i in range(self.board_size):
-            if(self.tictactoe_board[i][c_coord]!=player):
+            if self.tictactoe_board[i][c_coord] != player:
                 break
-            if i == self.board_size-1:
-                #player nyert
-        
-        #check diagonal
+            if i == self.board_size - 1:
+                print(player, ' nyert!')
+                return True
+
+        # check diagonal
         if r_coord == c_coord:
             for i in range(self.board_size):
-                if self.tictactoe_board[i][i]!=player:
+                if self.tictactoe_board[i][i] != player:
                     break
-                if i == self.board_size-1
-                    #player nyert
-
-        #check antidiagonal
-        if r_coord + c_coord == self.board_size-1:
-            for i in rang(self.board_size):
-                if self.tictactoe_board[i][self.board_size-1-i]!=player
+                if i == self.board_size - 1:
+                    print(player, ' nyert!')
+                    return True
+        # check antidiagonal
+        if r_coord + c_coord == self.board_size - 1:
+            for i in range(self.board_size):
+                if self.tictactoe_board[i][self.board_size - 1 - i] != player:
                     break
-                if i == self.board_size-1:
-                    #player nyert
-        
+                if i == self.board_size - 1:
+                    print(player, ' nyert!')
+                    return True
         if self.nof_moves == self.board_size ** 2:
-            #dontetlen
+            print('dontetlen')
+            return True
+
+        return False
+
+    def set_move(self, player, r_coord, c_coord):
+        self.tictactoe_board[r_coord][c_coord] = player
--- main.py
+++ main.py
@@ -1,20 +1,60 @@
-
 from Game import TicTacToe
 
 
-def main():
+def move_player(tictactoe, player):
+    free_position = False
+    while not free_position:
+        # row coordinate
+        valid_row_coord = False
+        while not valid_row_coord:
+            try:
+                r_coord = int(input('set the row position (1-3): ')) - 1
 
+                if r_coord < 0 or r_coord > 2:
+                    raise ValueError
+                valid_row_coord = True
+            except ValueError:
+                print('invalid input! try again...')
+
+        # column coordinate
+        valid_col_coord = False
+        while not valid_col_coord:
+            try:
+                c_coord = int(input('set the column position (1-3): ')) - 1
+
+                if c_coord < 0 or c_coord > 2:
+                    raise ValueError
+                valid_col_coord = True
+            except ValueError:
+                print('invalid input! try again...')
+
+        if tictactoe.check_position(r_coord, c_coord):
+            tictactoe.set_move(player, r_coord, c_coord)
+            tictactoe.print_board()
+            print()
+            if tictactoe.check_game(player, r_coord, c_coord):
+                return True
+            free_position = True
+        else:
+            print('reserved table position! please set again...')
+
+        return False
+
+
+def main():
     tictactoe = TicTacToe()
 
-    tictactoe.print_board()
+    while True:
+        print()
+        print('x player move...')
+        if move_player(tictactoe, 'x'):
+            break
 
-    tictactoe.set_move('x', 0, 0)
-
-    tictactoe.print_board()
+        print()
+        print('o player move...')
+        if move_player(tictactoe, 'o'):
+            break
 
 
 if __name__ == '__main__':
     main()
-
-
-
