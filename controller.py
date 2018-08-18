efc56a0dfe892c5d65148c387cb989470f086d79
stuksaa <stuksaa@gmail.com>
2018-08-18 21:29:35 +0200

    multiplayer + controller

---
+++ controller.py
@@ -0,0 +1,35 @@
+def check_valid_coord(dim):
+    # row coordinate
+    valid_coord = False
+    while not valid_coord:
+        try:
+            coord = int(input('set the ' + dim + ' position (1-3): ')) - 1
+
+            if coord < 0 or coord > 2:
+                raise ValueError
+            valid_coord = True
+        except ValueError:
+            print('invalid input! try again...')
+    return coord
+
+
+def move_player(tictactoe, player):
+    free_position = False
+    while not free_position:
+        # row coordinate
+        r_coord = check_valid_coord('row')
+
+        # column coordinate
+        c_coord = check_valid_coord('column')
+
+        if tictactoe.check_position(r_coord, c_coord):
+            tictactoe.set_move(player, r_coord, c_coord)
+            tictactoe.print_board()
+            print()
+            if tictactoe.check_game(player, r_coord, c_coord):
+                return True
+            free_position = True
+        else:
+            print('reserved table position! please set again...')
+
+        return False
--- main.py
+++ main.py
@@ -1,44 +1,5 @@
-from Game import TicTacToe
-
-
-def move_player(tictactoe, player):
-    free_position = False
-    while not free_position:
-        # row coordinate
-        valid_row_coord = False
-        while not valid_row_coord:
-            try:
-                r_coord = int(input('set the row position (1-3): ')) - 1
-
-                if r_coord < 0 or r_coord > 2:
-                    raise ValueError
-                valid_row_coord = True
-            except ValueError:
-                print('invalid input! try again...')
-
-        # column coordinate
-        valid_col_coord = False
-        while not valid_col_coord:
-            try:
-                c_coord = int(input('set the column position (1-3): ')) - 1
-
-                if c_coord < 0 or c_coord > 2:
-                    raise ValueError
-                valid_col_coord = True
-            except ValueError:
-                print('invalid input! try again...')
-
-        if tictactoe.check_position(r_coord, c_coord):
-            tictactoe.set_move(player, r_coord, c_coord)
-            tictactoe.print_board()
-            print()
-            if tictactoe.check_game(player, r_coord, c_coord):
-                return True
-            free_position = True
-        else:
-            print('reserved table position! please set again...')
-
-        return False
+from game import TicTacToe
+import controller
 
 
 def main():
@@ -47,12 +8,12 @@ def main():
     while True:
         print()
         print('x player move...')
-        if move_player(tictactoe, 'x'):
+        if controller.move_player(tictactoe, 'x'):
             break
 
         print()
         print('o player move...')
-        if move_player(tictactoe, 'o'):
+        if controller.move_player(tictactoe, 'o'):
             break
 
 
